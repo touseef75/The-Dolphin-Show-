6be318a53fc00ed9cec0fba1c778d9733b28a955
touseef75 <imrantouseef776@gmail.com>
2021-04-18 04:39:08 -0700

    fixed

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+let but = document.querySelector(".icon");
+
+
+$(".icon").click(function(){
+    $("ul").toggle(800);
+})
+// but.addEventListener("click",() => {
+// let ul = document.querySelector(".none")
+//     if(ul.style.display == "none"){
+//         ul.style.display = "block";
+//     }else{
+//         ul.style.display = "none";
+
+//     }
+// })
